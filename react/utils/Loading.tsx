6e98d8d5808b0c4d608e2b1fd9bf21ab1e1c8f41
Allysson Fernando Guedes de Almeida <allysson.guedes@trinto.com.br>
2022-07-19 15:13:40 -0300

    feat(custom-block): create hiring coins functionality

--- react/utils/Loading.tsx
+++
@@ -1,8 +0,0 @@
-import { CircleNotch } from 'phosphor-react';
-import React from 'react';
-
-const Loading: React.FC = () => {
-  return<CircleNotch size={32} color="#fff" />;
-}
-
-export default Loading;
